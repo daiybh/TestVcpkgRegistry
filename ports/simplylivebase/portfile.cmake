609db0be7dad57cedd2a6fc58481ab038fa8f7af
dai Li <daiybh@gmail.com>
2023-04-22 21:19:19 +0800

    Update simplylivebase

--- ports/simplylivebase/portfile.cmake
+++ ports/simplylivebase/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/simplylivebase.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF d327fd6aeef20db635763f402b7d67f60c359587
     HEAD_REF master    
 )
 
