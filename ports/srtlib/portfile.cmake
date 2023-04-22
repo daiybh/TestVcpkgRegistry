8ff459d437ced58a5263ca0917d5e93d5d795526
dai Li <daiybh@gmail.com>
2023-04-22 21:20:34 +0800

    Update srtlib

--- ports/srtlib/portfile.cmake
+++ ports/srtlib/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/srtlib.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 6c585451adfa1408650fb32d54f8f340d7e2abad
     HEAD_REF master    
 )
 
