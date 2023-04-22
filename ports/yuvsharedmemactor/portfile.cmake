480e4c51a34f4a98a516fd925781d822e6998f52
dai Li <daiybh@gmail.com>
2023-04-22 21:24:34 +0800

    Update yuvsharedmemactor

--- ports/yuvsharedmemactor/portfile.cmake
+++ ports/yuvsharedmemactor/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/yuvsharedmemactor.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 809de947d5d4086de7fb73c7bd5e95a3b7ed7bd5
     HEAD_REF master    
 )
 
