2a2a4989a8b0d81f9a77f3f37362e227c17eceff
dai Li <daiybh@gmail.com>
2023-04-22 20:00:22 +0800

    Update ajalibrarymodule

--- ports/ajalibrarymodule/portfile.cmake
+++ ports/ajalibrarymodule/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/ajalibrarymodule.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF eef9cca6c259c186d5eb0b4b4912b5b6bb28514f
     HEAD_REF master    
 )
 
