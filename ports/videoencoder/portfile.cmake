7f0963e8870af9c30704d949c43dc6388478ccb2
dai Li <daiybh@gmail.com>
2023-04-22 21:23:01 +0800

    Update videoencoder

--- ports/videoencoder/portfile.cmake
+++ ports/videoencoder/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/videoencoder.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 6d1104edae3d4bfb15d9861c6b6fac5640ced433
     HEAD_REF master    
 )
 
