5b150cc751e0bf18a7cb54717ed43a85a8169e4e
dai Li <daiybh@gmail.com>
2023-04-22 20:07:59 +0800

    Update feedtoai

--- ports/feedtoai/portfile.cmake
+++ ports/feedtoai/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/feedtoai.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 5ce19eefaa5211976bc0405962bfe84adf2bc8f4
     HEAD_REF master    
 )
 
