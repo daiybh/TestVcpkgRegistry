41916ad2f02c80e0d17e3348b038bb37005d892e
dai Li <daiybh@gmail.com>
2023-04-22 20:03:56 +0800

    Update deltacastframeconsumer

--- ports/deltacastframeconsumer/portfile.cmake
+++ ports/deltacastframeconsumer/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/deltacastframeconsumer.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 40cd0c95a41c4c8534694b4f839bbef27f9adec6
     HEAD_REF master    
 )
 
