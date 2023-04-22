751b2a5ae2a246477b5eac616db8cb4f247cdce3
dai Li <daiybh@gmail.com>
2023-04-22 20:08:09 +0800

    Update ffmpeg

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/ffmpeg.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 2930021a1e49643957d87a9191ed7a5c6cd72f63
     HEAD_REF master    
 )
 
