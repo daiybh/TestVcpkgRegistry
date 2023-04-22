316a8b3c187d04f0ab9781dbb294bdc1d9009c5b
dai Li <daiybh@gmail.com>
2023-04-22 20:01:03 +0800

    Update audioencdebuginfo

--- ports/audioencdebuginfo/portfile.cmake
+++ ports/audioencdebuginfo/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/audioencdebuginfo.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 7ebcd4f203bbda78dc43b6a1924926ddb23b3c02
     HEAD_REF master    
 )
 
