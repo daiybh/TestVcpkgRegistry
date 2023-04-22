d9075e11c8944950ca8806a89d4c11d02c929f9a
dai Li <daiybh@gmail.com>
2023-04-22 21:10:22 +0800

    Update localfileframeprovider

--- ports/localfileframeprovider/portfile.cmake
+++ ports/localfileframeprovider/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/localfileframeprovider.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 56a93272cc52340a22ca11930ee7374a140da879
     HEAD_REF master    
 )
 
