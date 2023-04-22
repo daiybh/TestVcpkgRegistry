618e773bae7c6682f9708f6e638de0339e702c06
dai Li <daiybh@gmail.com>
2023-04-22 21:14:34 +0800

    Update ndilib

--- ports/ndilib/portfile.cmake
+++ ports/ndilib/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/ndilib.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF f2f76d2a3e716e06ecc49c615314b7abc5d1dc23
     HEAD_REF master    
 )
 
