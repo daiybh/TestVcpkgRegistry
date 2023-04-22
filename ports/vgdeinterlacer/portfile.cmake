c502a38adf2a83ccb29bbc2ce7b4b8290d7a96d9
dai Li <daiybh@gmail.com>
2023-04-22 21:22:17 +0800

    Update vgdeinterlacer

--- ports/vgdeinterlacer/portfile.cmake
+++ ports/vgdeinterlacer/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/vgdeinterlacer.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 694dc7c5a249bfa93a9a10eeb6a05de14592b870
     HEAD_REF master    
 )
 
