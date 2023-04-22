d8baba87a984b2410661dd8ce99e001f7e6b1e13
dai Li <daiybh@gmail.com>
2023-04-22 21:15:37 +0800

    Update patterngeneratorprovider

--- ports/patterngeneratorprovider/portfile.cmake
+++ ports/patterngeneratorprovider/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/patterngeneratorprovider.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 212ab1541be2acacd125f29b48bcd859797a3342
     HEAD_REF master    
 )
 
