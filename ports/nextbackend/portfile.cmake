8a76bf31fa132eb8a47b3370e3bd5ee8653ee454
dai Li <daiybh@gmail.com>
2023-04-22 21:15:05 +0800

    Update nextbackend

--- ports/nextbackend/portfile.cmake
+++ ports/nextbackend/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/nextbackend.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 4a88579b5258914c824ebd3a1864355418cc4835
     HEAD_REF master    
 )
 
