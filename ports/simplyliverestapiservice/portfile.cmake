e90372bef2af90781c6620f1efaf216ad8c1c976
dai Li <daiybh@gmail.com>
2023-04-22 21:19:33 +0800

    Update simplyliverestapiservice

--- ports/simplyliverestapiservice/portfile.cmake
+++ ports/simplyliverestapiservice/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/simplyliverestapiservice.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 1b64a50f3a6da93f383efdae8e7fb196541d50e4
     HEAD_REF master    
 )
 
