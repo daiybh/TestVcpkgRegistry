3e68ca3f5ac1eae375ea42d28241011e3e8ad12d
dai Li <daiybh@gmail.com>
2023-04-22 20:02:01 +0800

    Update blackmagiclib

--- ports/blackmagiclib/portfile.cmake
+++ ports/blackmagiclib/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/blackmagiclib.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF dea2cba168fd15ad57079856f094ca3efc7274a6
     HEAD_REF master    
 )
 
