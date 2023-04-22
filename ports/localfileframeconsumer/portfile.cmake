2ec43da7c752e0453fa9725f6ab218326a0e32b0
dai Li <daiybh@gmail.com>
2023-04-22 21:05:19 +0800

    Update localfileframeconsumer

--- ports/localfileframeconsumer/portfile.cmake
+++ ports/localfileframeconsumer/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/localfileframeconsumer.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 8a89603770d4ba5919217d6c54cc51a048436669
     HEAD_REF master    
 )
 
