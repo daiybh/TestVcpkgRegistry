cc1d4e6c3a6e4e7ba919074b93004701fe61003b
dai Li <daiybh@gmail.com>
2023-04-22 21:23:49 +0800

    Update videoframegpu

--- ports/videoframegpu/portfile.cmake
+++ ports/videoframegpu/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/videoframegpu.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF fef133e260df72621c798846608515aaf2b7e39f
     HEAD_REF master    
 )
 
