d84481a9c50eee802a10066f0fe1a5783a737556
dai Li <daiybh@gmail.com>
2023-04-22 21:16:20 +0800

    Update rapidjson

--- ports/rapidjson/portfile.cmake
+++ ports/rapidjson/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/rapidjson.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF ca187bdb84e086e169d7fe28997292cb75f5e563
     HEAD_REF master    
 )
 
