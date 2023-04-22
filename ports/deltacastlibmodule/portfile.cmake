0918bbfc23ba077ec309229671aa080fd585ea62
dai Li <daiybh@gmail.com>
2023-04-22 20:04:16 +0800

    Update deltacastlibmodule

--- ports/deltacastlibmodule/portfile.cmake
+++ ports/deltacastlibmodule/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/deltacastlibmodule.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 15eef54828c27e3f1e73efd742fecdbc7655cb3b
     HEAD_REF master    
 )
 
