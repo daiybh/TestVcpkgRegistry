19158f6dd1921f7d98a0de78afa40e916c394f83
dai Li <daiybh@gmail.com>
2023-04-22 21:12:47 +0800

    Update mellanoxframeprovider

--- ports/mellanoxframeprovider/portfile.cmake
+++ ports/mellanoxframeprovider/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/mellanoxframeprovider.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 1f1aaabcad574cbedbcf9d652c5db294dcbfd8a9
     HEAD_REF master    
 )
 
