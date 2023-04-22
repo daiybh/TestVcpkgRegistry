dbc3aabc23a8b17e061fcb141ef4e212c6c32699
dai Li <daiybh@gmail.com>
2023-04-22 21:17:18 +0800

    Update sdkframeconsumer

--- ports/sdkframeconsumer/portfile.cmake
+++ ports/sdkframeconsumer/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/sdkframeconsumer.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 2275ffed703cbe2cdecf5553613bee930263d9b7
     HEAD_REF master    
 )
 
