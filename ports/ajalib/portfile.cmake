91c46ef1bcd0d27e75c249d70209bd73c51dff2c
dai Li <daiybh@gmail.com>
2023-04-22 20:00:02 +0800

    Update ajalib

--- ports/ajalib/portfile.cmake
+++ ports/ajalib/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/ajalib.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 0f9ed0673b4f0d8ea27a34fbdc6218e0fb21e1bc
     HEAD_REF master    
 )
 
