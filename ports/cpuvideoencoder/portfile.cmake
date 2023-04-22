0636f899e5783a301ad569a8e06912b2273ce273
dai Li <daiybh@gmail.com>
2023-04-22 20:03:37 +0800

    Update cpuvideoencoder

--- ports/cpuvideoencoder/portfile.cmake
+++ ports/cpuvideoencoder/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/cpuvideoencoder.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 08a142b330bac427e9663e749328004685824383
     HEAD_REF master    
 )
 
