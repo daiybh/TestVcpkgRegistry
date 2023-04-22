2670e7f8fa3de0c18d59b23eb6c854b4565da310
dai Li <daiybh@gmail.com>
2023-04-22 21:20:03 +0800

    Update srtframeconsumer

--- ports/srtframeconsumer/portfile.cmake
+++ ports/srtframeconsumer/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/srtframeconsumer.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 29deebe2020fea2c13409ff2bce508ad99bd37a7
     HEAD_REF master    
 )
 
