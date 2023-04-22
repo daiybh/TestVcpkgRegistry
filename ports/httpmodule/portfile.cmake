dd1da6e68788f26e6615e488bd060637cbe1c06c
dai Li <daiybh@gmail.com>
2023-04-22 20:18:12 +0800

    Update httpmodule

--- ports/httpmodule/portfile.cmake
+++ ports/httpmodule/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/httpmodule.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 9820c8822f7877b81e0924bed4a7503c08f34700
     HEAD_REF master    
 )
 
