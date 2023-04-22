b04c647981d46982fe531aa9595aa65a7ed5cac6
dai Li <daiybh@gmail.com>
2023-04-22 21:13:32 +0800

    Update mosaicframeprovider

--- ports/mosaicframeprovider/portfile.cmake
+++ ports/mosaicframeprovider/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/mosaicframeprovider.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 414fb8c283014badd876347dcf28a536fad3b74f
     HEAD_REF master    
 )
 
