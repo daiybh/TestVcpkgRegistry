5a9e0e7f39b50417b2ee0ca933e7acb527085219
dai Li <daiybh@gmail.com>
2023-04-22 21:16:49 +0800

    Update restservicebase

--- ports/restservicebase/portfile.cmake
+++ ports/restservicebase/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/restservicebase.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 3bd93eefcfa4fefd7da83bb26067b3d6b0f01944
     HEAD_REF master    
 )
 
