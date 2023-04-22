d69c1014c928a1e98b940a97bd0b89514e4e83a2
dai Li <daiybh@gmail.com>
2023-04-22 20:01:23 +0800

    Update audioframedec

--- ports/audioframedec/portfile.cmake
+++ ports/audioframedec/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/audioframedec.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF fe48ab5143911887b1e14ee580ee5baa786cbc5d
     HEAD_REF master    
 )
 
