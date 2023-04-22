e423e72d215d133e9937ebd5fdc4b7cff24b18a0
dai Li <daiybh@gmail.com>
2023-04-22 21:18:20 +0800

    Update sharedmemhost

--- ports/sharedmemhost/portfile.cmake
+++ ports/sharedmemhost/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@bitbucket.org:id4tv/sharedmemhost.git
-    REF eeefe7bd5c987bd34b7aeb6670b2d656471ed732
+    REF 70e154a4c8137142876254184d9543ffd9e60fa1
     HEAD_REF master    
 )
 
